66b07d179458ac75b90974f9df63e54db261f871
Anurag chamoli <anuragchamoli2003@gmail.com>
2026-09-01 20:49:04 +0530

    fix animation

--- src/Component/Carts/Herocards.jsx
+++ src/Component/Carts/Herocards.jsx
@@ -4,17 +4,30 @@ function Herocards(){
     return(<>
     <div className={style.hercontainer}>
 
-    </div>
     <div className={style.herocard}>
         <h2>Flat 40% of in thi moonsoon season</h2>
     </div>
+
     <div className={style.herocard}>
         <h2>Big deal in this month</h2>
         <p>up to 35% of</p>
     </div>
+
      <div className={style.herocard}>
         <h2>Fashion sale </h2>
         <p>up to 30% of</p>
+    </div>
+
+    <div className={style.herocard}>
+        <h2>Spacial deal in home decore</h2>
+        <p>up to 30%-50% of</p>
+        <p>buy now</p>
+    </div>
+
+     <div className={style.herocard}>
+        <h2>Flat 40% of in thi moonsoon season</h2>
+    </div>
+
     </div>
     </>
     )
